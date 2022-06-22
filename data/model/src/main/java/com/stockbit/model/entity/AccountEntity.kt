b6b5fc604ed194bd29cffc8ff7d70018cf6fc041
Sona Setiana <sonasetiana13@gmail.com>
2022-06-22 08:34:08 +0700

    create account entity and model

---
+++ data/model/src/main/java/com/stockbit/model/entity/AccountEntity.kt
@@ -0,0 +1,35 @@
+package com.stockbit.model.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.stockbit.model.Account
+
+@Entity(tableName = "Account")
+data class AccountEntity(
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: String,
+
+    @ColumnInfo(name = "username")
+    val username: String,
+
+    @ColumnInfo(name = "email")
+    val email: String,
+
+    @ColumnInfo(name = "phone")
+    val phone: String,
+
+    @ColumnInfo(name = "password")
+    val password: String
+
+) {
+    constructor(model: Account) : this (
+        id = model.id,
+        username = model.username,
+        email = model.email,
+        phone = model.phone,
+        password = model.password
+    )
+}
